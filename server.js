b7d98a08675076854ff616cc26e240eab9a6a357
rawdhadaikhi <75029891+rawdhadaikhi@users.noreply.github.com>
2021-03-05 10:27:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,28 +23,7 @@ app.use(verify);
 
 app.use(express.static('views'));
 //*************************************************************************** */
-// router.get('/', (req, res,next) =>{
-//     res.sendFile(path.join(__dirname, './views/index.html'));
-//    next();
-// })
 
-// // go to services page
-// router.get('/*', (req, res,next) =>{
-//     res.sendFile(path.join(__dirname, './views/Services.html'));
-//    next()
-// })
-
-// //go to contact page
-// router.get('/', (req, res) =>{
-//     res.sendFile(path.join(__dirname, './views/ContactUs.html'));
-   
-// })
-
-// //redirect error page
-// router.get('/', (req, res) =>{
-//     res.sendFile(path.join(__dirname, './views/Error.html'));
-// })
-//*********************************************************************** */
 
 app.listen(port,(err)=>{
     err ? console.log(err) : console.log('the server is runnig on port 5000..')
